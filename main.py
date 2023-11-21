306ce3e7dbebb37ae120d99af2d9866d248554c6
gromboy <timazaharov07@gmail.com>
2023-11-21 14:25:49 +0400

    70%

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import os
 import platform
-import sys
 import time
 from datetime import datetime
 from pprint import pprint
-import subprocess
 import psutil
 import cpuinfo
+import wmi
 
 start_time = time.time()
 
@@ -15,9 +13,13 @@ start_time = time.time()
 def general_info():
     result = {}
     inf = platform.uname()
-    result['win'] = inf.system + inf.release + ' ' + inf.version + ' ' + inf.machine
-    result['name'] = inf.node
-    result['proc'] = process_inf()['name']
+    result['Версия OC'] = inf.system + inf.release + ' ' + inf.version + ' ' + inf.machine
+    result['Имя компьютера'] = inf.node
+    result['Процессор'] = process_inf()['Название']
+    result['Видеокарта'] = [i.Name for i in wmi.WMI().Win32_VideoController()]
+    c = wmi.WMI()
+    for bios_id in c.Win32_BIOS():
+        result["BIOS"] = bios_id.SerialNumber.strip()
     return result
 
 
@@ -26,17 +28,17 @@ def process_inf(need_all=True):
     result = {}
     if need_all:
         processor_name = cpuinfo.get_cpu_info()['brand_raw']
-        result['name'] = processor_name
+        result['Название'] = processor_name
         architecture = platform.architecture()[0]
-        result['arch'] = architecture
+        result['Архитектура'] = architecture
     cores = psutil.cpu_count()
-    result['log_cores'] = cores
-    result['freq'] = psutil.cpu_freq().current
+    result['Логических процессоров'] = cores
+    result['Тактовая частота'] = str(psutil.cpu_freq().current) + ' Ггц'
     cores = []
     for percentage in psutil.cpu_percent(percpu=True, interval=1):
         cores.append(percentage)
-    result['cores'] = cores
-    result['usage'] = psutil.cpu_percent()
+    result['Ядра'] = cores
+    result['Использование'] = psutil.cpu_percent()
     return result
 
 
@@ -70,6 +72,18 @@ def disk():
     return result
 
 
-# pprint(general_info())
-print("--- %s seconds ---" % round(time.time() - start_time, 5))
-pprint(disk())
+def op():
+    result = {}
+    n = psutil.virtual_memory()
+    result['vsego'] = translate(n.total)
+    result['dostupnaya'] = translate(n.available)
+    result['procent zanyato'] = str(n.percent) + '%'
+    result['ispolzuetsya'] = translate(n.used)
+    return result
+
+
+if __name__ == '__main__':
+    pprint(process_inf())
+    print("--- %s seconds ---" % round(time.time() - start_time, 5))
+
+# pip install -r requirements.txt
--- window.py
+++ window.py
@@ -1,16 +1,74 @@
+import sys
+
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+from UI import Ui_MainWindow
+from PyQt5.QtGui import QIcon
 from main import *
 
 
-class Main(QMainWindow):
+class Main(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
+        # uic.loadUi('UI.ui', self)
+        self.setupUi(self)
+        self.setWindowIcon(QIcon('icon.ico'))
+        self.status = QLabel(self)
+        self.statusbar.addWidget(self.status)
+        self.now = self.general
+        self.btn.clicked.connect(lambda k: self.now())
+        self.general()
+
+        self.action6.triggered.connect(self.general)
+        self.action.triggered.connect(self.timeon)
+        self.action2.triggered.connect(self.proc)
+
+    def proc(self):
+        self.progress.setValue(20)
+
+        def correct_cores(arr):
+            for i, j in enumerate(arr):
+                yield f'Ядро {i + 1} - {j}%'
+
+        text = ''''''
+        inf = process_inf()
+        for i in inf:
+            if i == 'Ядра':
+                text += i + ':\n' + '\n'.join(correct_cores(inf[i])) + '\n'
+            else:
+                text += i + ' - ' + str(inf[i]) + '\n'
+        self.progress.setValue(100)
+        self.status.setText('Процессор')
+        self.now = self.proc
+        self.place.setText(text)
+
+    def timeon(self):
+        self.progress.setValue(20)
+        time.sleep(0.02)
+        self.progress.setValue(100)
+        self.place.setText(time_on())
+        self.now = self.timeon
+        self.status.setText('Время включения')
+
+    def general(self):
+        self.progress.setValue(20)
+        text = ''''''
+        inf = general_info()
+        for i in inf:
+            text += i + ' - ' + str(inf[i]) + '\n'
+        self.progress.setValue(100)
+        self.status.setText('Общая информация')
+        self.now = self.general
+        self.place.setText(text)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     admin_app = Main()
     admin_app.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec())
